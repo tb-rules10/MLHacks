802965cd0fe4e9e595a6601d8dbd3c71692f4301
tb-rules10 <58645688+tb-rules10@users.noreply.github.com>
2021-10-14 12:46:29 +0530

    Update ThemeSong.py

--- Music Player/ThemeSong.py
+++ Music Player/ThemeSong.py
@@ -1,1 +1,10 @@
+import time
+from pygame import mixer
+
+
+mixer.init()
+mixer.music.load("game-of-thrones-theme-song-ringtone-30782.mp3")
+mixer.music.play()
+while mixer.music.get_busy():  # wait for music to finish playing
+    time.sleep(1)
 
