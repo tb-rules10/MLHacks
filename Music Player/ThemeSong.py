5a50f64042cad52263deeb982d5e60cd4369bfe2
tb-rules10 <58645688+tb-rules10@users.noreply.github.com>
2021-10-14 18:55:51 +0530

    Update ThemeSong.py

--- Music Player/ThemeSong.py
+++ Music Player/ThemeSong.py
@@ -1,10 +1,26 @@
 import time
 from pygame import mixer
 
+print("\nTB's Music Player\n")
+
+d1 = {1: "Tu Aake Dekhle", 2: "Ranjha", 3: "Ek Raat",
+      4: "Tum Kyu Chale Aate Ho", 5: "Mann Mera", 6:"Beggin", 7: "Venom",
+      8: "What a Time", 9: "Arcade", 10: "GOT-Theme Song"}
+
+for key, value in d1.items():
+    print ("{} \t {}".format(key,value))
+
+x = int(input("\nEnter Your Song - "))
+str = d1[x] + ".mp3"
+print(str)
 
 mixer.init()
-mixer.music.load("game-of-thrones-theme-song-ringtone-30782.mp3")
+mixer.music.load(str)
+print("Playing :", end=" ")
 mixer.music.play()
-while mixer.music.get_busy():  # wait for music to finish playing
-    time.sleep(1)
 
+print ("Enter 0 to stop")
+while mixer.music.get_busy():
+    exit(input())# wait for music to finish playing
+    time.sleep(1)
+    
