44d6f87ebf5df389c9bcf8cd58ac2ac720f056fe
tb-rules10 <58645688+tb-rules10@users.noreply.github.com>
2021-10-15 02:27:02 +0530

    Create Savings_Calculator.py

---
+++ Savings_Calculator.py
@@ -0,0 +1,12 @@
+print("Your Savings Calculator")
+sal = int(input("How much do you make per hour - "))
+hrs = 8
+
+daily_savings = sal*hrs
+print("This is your Daily Savings"+daily_savings)
+
+weekly_savings = sal*hrs*5
+print("This is your Daily Savings"+weekly_savings)
+
+monthly_savings = sal*hrs*5*4
+print("This is your Daily Savings"+monthly_savings
