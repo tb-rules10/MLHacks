ac329ac568d79ede95c831efbbb9c74e17fb4361
tb-rules10 <58645688+tb-rules10@users.noreply.github.com>
2021-10-17 18:52:28 +0530

    Update Code.ino

--- RGB LED - Colour Mixing/Code.ino
+++ RGB LED - Colour Mixing/Code.ino
@@ -12,12 +12,12 @@ int r = 0;
 int g = 0;
 int b = 0;
 void loop() {
-  r = random(0, 255);     # to store a random colour
+  r = random(0, 255);     // to store a random colour
   g = random(0, 255);
   b = random(0, 255);
   analogWrite(LEDR, r);     
   analogWrite(LEDG, g);
   analogWrite(LEDB, b);
   delay(1000);
-  # above lines will make the rgb glow in different colours
+  /* above lines will make the rgb glow in different colours */
 }
